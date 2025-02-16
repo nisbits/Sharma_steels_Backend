b7564ce4e5870feaadca47824caf094fb7e2aa0d
nisbits <nishantnv267@gmail.com>
2025-02-17 01:16:57 +0530

    static files

--- Sharma_steels/settings.py
+++ Sharma_steels/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/django-static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "django-static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
